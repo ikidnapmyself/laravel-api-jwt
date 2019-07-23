60541fda5373fdf05f2e9b206295227d3f7dadeb
ikidnapmyself <burakalbayrak@live.com>
2019-07-23 20:52:33 +0300

    email unique

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name'     => 'required|min:3',
-            'email'    => 'required',
+            'email'    => 'required|unique:users',
             'password' => 'required|min:6',
         ];
     }
